85422eff42a80a1471e0d0b6f7d3e9f19080c9dc
khaled Assidi <Kaa502@students.bau.edu.lb>
2024-09-01 18:13:59 +0300

    first commit

---
+++ app/src/main/java/com/example/intuitlibrary/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.intuitlibrary;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+/*
+
+la7 ne7ke 3an library est5dema kteer bel android esma intuit
+library hay btet3emal ma3 shshet l met3aded bel android b ma3na ana l youm bade 2a3mel app l app heda fe tasmem m3yan heda tasmem me7tej eno yeshte8l 3ala aktr mn 7ajm shshe ya3ne yetla3 zabet 3ala kel anwa3 shshet iza kenet z8ere kbere x large..etc
+ne7na mn3ref eno l screen size bye5telef mn device la device fa heda she b2de eno bye5telf tasmem 2yeseto tyeb keef momken 7el l meshkle ana 3ashn ykoun l tasmem ta3ele zabet 3ala kel devices
+
+ne7na iza mnetzakr kenet tare2a 3ashn na3ml tasmem la aktr mn shshe heye best3mel dimens na3mel aktr mn file dimens wa7de iza keen hshet x large wa7de large wa7de small w hay tare2a sa7e7a
+
+ba3den tele3 library ra7yetna heye 3mlet nafs mabda2 dimens bas heye 3mleto w jahazeto w 3mlet files kteer la a8lab shshet l most5dme w 7ateta bel library hay bshkl eno tsahel 3lye este5dema esma l library hay intuit
+
+fa 2awl she bade 7ot hay library 3ande bel gradle le esma intuit
+
+fa ana ba3mel tasmem xml hala2 2awl she bas hala2 badel ma est5dem dp best5dem wa7det l 2eyes le esma sdp keef ya3ne:
+ya3ne 3ande ana hala2 tasmem fe soura w textview w 2 button
+hala2 l image view 7ateta ana 200 dp width w 200dp height fa library hay te7kelak badel ma t7ot 200 dp 7ot 200sdp
+fa byet3lk @dimen/200sdp fa hay ma3neta 7a nl2e eno library hay m5zneby defult lista mn kel l a7jem 2aw a8lab l a7jeb le enta momekn test5dema ya3ne btshof eno iza keen shshe l width tab3a 1080dp st5dem mesh l 200dp st5dem 720dp
+l shshe l 300dp st5dem l 200dp l 390 msln 220dp heye hay library m3rfe malefet dimens generic feha kel a7jem shshe
+ya3ne m3refe kel l ara2m w l 2eme le edema heye l eme le bten7at 7asab shshe le enta 3am teshte8l 3lyha
+fa enta msln st5dmt width 200spd w height sdp la image fa btseer tshof hay library iza enta 3am test5dem shshe kbere hay l 200 dp 7aela la 720dp iza enta 3am test5dem shhe small 5ale l 200dp msln 100 dp msln heek
+ya3ne enta 3am testd3e eme mawjoude b file dimens w heye la7ala hay l eme btet8ayr 7asb shshe w 7ajma la shshe
+
+
+hala2 bel nesbe la ta3moul ma3 text metl ma taf2na abl eno bte3temed 3ala l sp fa 3mleen mnafs library hay nos5a lal sp ya3ne hounek kenet kola la dp hala2 houn lal sp
+fa hala2 badel ma nest5dem sp mnseer mnest5dem ssp
+
+ya3ne sar 3ande badel l dp-> sdp
+w bade l sp->ssp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
